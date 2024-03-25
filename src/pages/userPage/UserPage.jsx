0aaeaf88b70c5644700d0da5cb6a7036964b5bf1
agus <aalma@mail.austral.edu.ar>
2024-03-25 19:04:25 -0300

    userPage no progress

--- src/pages/userPage/UserPage.jsx
+++ src/pages/userPage/UserPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './UserPage.css';
+import {Link} from "react-router-dom";
 
 
 
@@ -11,9 +12,13 @@ function UserPage() {
     return (
         <div className='main-page-format'>
             <div className='bg-white p-4 rounded w-25'>
-                <h2>User Page</h2>
-                <div className="bottom-right-button-container">
-                    <button type='submit'  id="deleteAccountButton" className='btn btn-success w-100 rounded-0'> Delete My Account
+                <Link to="/home" className='btn btn-default border w-20 bg-light rounded-0 text-decoration-none'>Home</Link>
+                <div>
+                    <h2 className="name-container"> getUserName()</h2>
+                </div>
+
+                <div>
+                    <button type='submit'  id="deleteAccountButton" className='btn btn-carefull w-100 rounded-0'> Delete My Account
                     </button>
                 </div>
             </div>
