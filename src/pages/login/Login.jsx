522ebcab2090b8a0d2b823bca763a26ac43bf1a9
agus <aalma@mail.austral.edu.ar>
2024-04-02 19:52:22 -0300

    cambio de estetics

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import Validation from './LoginValidation';
 import axios from 'axios';
 import './Login.css';
+import '../../styles.css'
 
 
 function Login() {
@@ -36,23 +37,27 @@ function Login() {
     }
 
     return (
-        <div className='loginPage'>
-            <h2>Log-In</h2>
+        <div className='main-page'>
+            <h2 className='main-page-header'>Log-In</h2>
+
             <form action="" onSubmit={handleSubmit}>
-                <div className='mb-3'>
-                    <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' name='email'
-                        onChange={handleInput} className='form-control rounded 0'/>
+
+                <div id='email-prompt' className='prompt'>
+                    <label id='top-text' htmlFor="email"><strong>Email</strong></label>
+                    <input type="email" placeholder='Enter Email' name='email' onChange={handleInput} className='form-control rounded 0'/>
                     {errors.email && <span className='text-danger'> {errors.email}</span>}
                 </div>
-                <div className='mb-3'>
-                    <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' name='password'
-                        onChange={handleInput} className='form-control rounded 0'/>
+
+                <div id='pass-prompt' className='prompt'>
+                    <label id='top-text' htmlFor="password"><strong>Password</strong></label>
+                    <input type="password" placeholder='Enter Password' name='password' onChange={handleInput} className='form-control rounded 0'/>
                     {errors.password && <span className='text-danger'> {errors.password}</span>}
                 </div>
-                <button type='submit'  className='btn btn-success w-100 rounded-0'>Log in</button>
+
+                <button type='submit'  id='colouredButton' >Log in</button>
+
                 <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
+
             </form>
         </div>
     )
