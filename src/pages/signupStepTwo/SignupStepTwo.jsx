e51c99c18d0a80e0fd6b489a40ff751ca9ee96b3
agus <aalma@mail.austral.edu.ar>
2024-04-02 00:04:12 -0300

    SignupStepTwo.jsx a mirar por problemas de merge

--- src/pages/signupStepTwo/SignupStepTwo.jsx
+++ src/pages/signupStepTwo/SignupStepTwo.jsx
@@ -66,43 +66,6 @@ function SignupStepTwo() {
                 <p>By clicking here you agree with our terms of service.</p>
                 <button type='submit' onClick={handleSubmit} className='btn btn-success w-100 rounded-0'>Join the Club!</button>
             </form>
-        <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-            <div className='bg-white p-4 rounded w-100'>
-                <Link to="/signup" className='btn btn-default border w-20 bg-light rounded-0 text-decoration-none'>Back</Link>
-                <h2>Sign-Up</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='mb-3'>
-                        <label htmlFor="age"><strong>Age</strong></label>
-                        <input type="number" placeholder='Enter Age' name='age' value={valuesStepTwo.age}
-                               onChange={handleInput} className='form-control rounded 0' />
-                        {errors.age && <span className='text-danger'> {errors.age}</span>}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="weight"><strong>Weight</strong></label>
-                        <input type="number" placeholder='Enter Weight (kg)' name='weight' value={valuesStepTwo.weight}
-                               onChange={handleInput} className='form-control rounded 0' />
-                        {errors.weight && <span className='text-danger'> {errors.weight}</span>}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="height"><strong>Height</strong></label>
-                        <input type="number" placeholder='Enter Height (cm)' name='height' value={valuesStepTwo.height}
-                               onChange={handleInput} className='form-control rounded 0' />
-                        {errors.height && <span className='text-danger'> {errors.height}</span>}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="gender"><strong>Gender</strong></label>
-                        <select name="gender" value={valuesStepTwo.gender} onChange={handleInput} className='form-control rounded-0'>
-                            <option value="">Select Gender</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="other">Other</option>
-                        </select>
-                        {errors.gender && <span className='text-danger'> {errors.gender}</span>}
-                    </div>
-                    <p>By clicking here you agree with our terms of service.</p>
-                    <button type='submit' className='btn btn-success w-100 rounded-0'>Join the Club!</button>
-                </form>
-            </div>
         </div>
     )
 }
