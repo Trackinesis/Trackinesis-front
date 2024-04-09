7bc478a2efbf2d7979bd231174455df0bfbfed84
AAgustin9 <aalma@mail.austral.edu.ar>
2024-04-09 12:37:50 -0300

    delete button account

--- src/pages/userPage/UserPage.jsx
+++ src/pages/userPage/UserPage.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './UserPage.css';
 import '../../styles.css'
 
@@ -9,17 +10,31 @@ function UserPage() {
         name: '',
         email: '',
         password: '',
+        id: '',
     });
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setUser((prevState) => ({ ...prevState, [name]: value }));
     };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        /* Aca deberia esta la llamada a bd para guardar el usuario*/
-    };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const userId = user.id
+
+        try {
+            const API_URL = `http://localhost:8081/user/${userId}`
+            const res = await axios.delete(API_URL);
+
+            if (res.status === 200) {
+                console.log('User deleted successfully');
+            } else {
+                console.log('Error deleting user');
+            }
+        } catch (error) {
+            console.log('Error deleting user', error);
+        }
+    };
 
     return (
         <div className='main-page'>
