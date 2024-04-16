f303030d11dda99d835bf287811075c00ebc56c8
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-04-16 10:42:59 -0300

    front update

--- src/pages/signupStepTwo/SignupStepTwo.jsx
+++ src/pages/signupStepTwo/SignupStepTwo.jsx
@@ -18,15 +18,16 @@ function SignupStepTwo() {
         setValues(prev => ({...prev, [event.target.name]: event.target.value}))
     }
 
-    const handleSubmit =(event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         setErrors(Validation(valuesStepTwo));
-        if (errors.age === "" && errors.weight === "" && errors.height === "" && errors.gender === "") {
-            axios.post('http://localhost:8081/signupsteptwo', valuesStepTwo)
-            .then(res => {
-                navigate('/');
-            })
-            .catch(err => console.log(err));
+        const userId = localStorage.getItem('userId');
+        if (errors.age === "" && errors.weight === "" && errors.height === "" && errors.gender === "" && userId) {
+            axios.post('http://localhost:8081/signupsteptwo', {...valuesStepTwo, userId})
+                .then(res => {
+                    navigate('/');
+                })
+                .catch(err => console.log(err));
         }
     }
 
