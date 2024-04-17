d08ee6ff6233aba5b4245fbdce2a198ec7b23765
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-04-16 22:24:58 -0300

    changes in createNewExcercise, css and jsx

--- src/pages/createPlan/createNewExercise.jsx
+++ src/pages/createPlan/createNewExercise.jsx
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Validation from './SignupValidation';
 import axios from 'axios';
-import './Signup.css';
 import '../../styles.css'
+import './createNewExcercise.css'
 
-function createNewExercise(){
+function createNewExercise(props){
+    const { planName } = props.location.state;
 
+    return (
+        <div className='create-new-exercise'>
+            <h2>Create New Exercise</h2>
+            <p>Plan name: {planName}</p>
+                <div className='prompt'>
+                    <label id='top-text' htmlFor="exercise name"><strong>Exercise name:</strong></label>
+                    <input id='formsInput' type="exercise name" placeholder='Enter exercise name:' name='plan name' />
+                </div>
+        </div>
+    );
 
 }
