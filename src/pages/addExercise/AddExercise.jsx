89a0956b37fa3957807cd5006821dd907a4a4d35
agus <aalma@mail.austral.edu.ar>
2024-04-22 17:58:10 -0300

    AddExercise.jsx

--- src/pages/addExercise/AddExercise.jsx
+++ src/pages/addExercise/AddExercise.jsx
@@ -9,9 +9,10 @@ function AddExercise() {
     const [errors, setErrors] = useState({});
     const [valuesExercise, setValuesExercise] = useState({
         name: '',
-        type: '',
-        description: '',
-        time: '',
+        sets: '',
+        reps: '',
+        weight: '',
+        duration: '',
     });
     const [exerciseOptions, setExerciseOptions] = useState([]);
     const [selectedExercise, setSelectedExercise] = useState('');
@@ -30,7 +31,7 @@ function AddExercise() {
     const handleExerciseTypeChange = (value) => {
         setExerciseType(exerciseType === value ? null : value);
     }
-    const handleTypeInput = (event) => {
+    const handleInput = (event) => {
         setValuesExercise(prev => ({ ...prev, [event.target.name]: event.target.value}))
     }
 
@@ -54,14 +55,14 @@ function AddExercise() {
             <form action="" onSubmit={handleSubmitNewExercise}>
                 <div className='mb-3'>
                     <label id='top-text' htmlFor="exercise name"><strong>Exercise name:</strong></label>
-                    <select name="name" onChange={handleTypeInput} id='formsInput'>
+                    <select name="name" onChange={handleInput} id='formsInput'>
                         <option disabled selected value="">Select Exercise</option>
                         {exerciseOptions.map((exercise, index) => (
                             <option key={index} value={exercise}>{exercise}</option>
                         ))}
                     </select>
                     {errors.name && <span className='text-danger'> {errors.name}</span>}
-                    <OptionsForExercise selectedExercise={exerciseType} handleExerciseClick={handleExerciseTypeChange} valuesExercise={valuesExercise} handleTypeInput={handleTypeInput} />
+                    <OptionsForExercise selectedExercise={exerciseType} handleExerciseClick={handleExerciseTypeChange} valuesExercise={valuesExercise} handleTypeInput={handleInput} />
                 </div>
                 <div className='prompt'>
                     <button id='createExercise' type="submit">Create exercise</button>
@@ -73,7 +74,7 @@ function AddExercise() {
 
 export default AddExercise;
 
-function OptionsForExercise({selectedExercise, handleExerciseClick, valuesExercise, handleTypeInput}) {
+function OptionsForExercise({selectedExercise, handleExerciseClick, valuesExercise, handleInput}) {
     const exerciseOptions = ['Exercise with sets', 'Exercise with time'];
     const convertSecondsToMinutes = (seconds) => {
         const totalSeconds = parseInt(seconds);
@@ -101,17 +102,17 @@ function OptionsForExercise({selectedExercise, handleExerciseClick, valuesExerci
                     <div className='prompt'>
                         <label id='top-text' htmlFor="exercise sets"><strong>Number of sets:</strong></label>
                         <input id='formsInput' type="exercise sets" placeholder='Enter number of sets:'
-                               name='exercise sets'/>
+                               name='sets' onChange={handleInput}/>
                     </div>
                     <div className='prompt'>
                         <label id='top-text' htmlFor="exercise reps"><strong>Number of reps:</strong></label>
                         <input id='formsInput' type="exercise reps" placeholder='Enter number of reps:'
-                               name='exercise reps'/>
+                               name='reps' onChange={handleInput}/>
                     </div>
                     <div className='prompt'>
                         <label id='top-text' htmlFor="exercise weight"><strong>Weight:</strong></label>
                         <input id='formsInput' type="exercise weight" placeholder='Enter weight:'
-                               name='exercise weight'/>
+                               name='weight' onChange={handleInput}/>
                     </div>
                 </>
             )}
@@ -123,7 +124,7 @@ function OptionsForExercise({selectedExercise, handleExerciseClick, valuesExerci
                             <span>{convertSecondsToMinutes(valuesExercise.time)}</span>
                         </label>
                         <input id='formsInput' type="number" placeholder='Enter duration:'
-                               name='time' value={valuesExercise.time} onChange={handleTypeInput} />
+                               name='duration' value={valuesExercise.time} onChange={handleInput} />
                     </div>
                 </>
             )}
