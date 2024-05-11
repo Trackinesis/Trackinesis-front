dc4c61b0fc00e39405bbc77e6210106559b57d22
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-05-11 15:46:42 -0300

    changes

--- src/pages/addExercise/AddExercise.jsx
+++ src/pages/addExercise/AddExercise.jsx
@@ -28,21 +28,29 @@ function AddExercise() {
             .catch(err => console.log(err));
     }, []);
 
-    const handleExerciseTypeChange = (value) => {
-        setExerciseType(exerciseType === value ? null : value);
-    }
+    const handleExerciseTypeChange = (type) => {
+        setExerciseType(type);
+        setValuesExercise({
+            name: '',
+            sets: '',
+            reps: '',
+            weight: '',
+            duration: '',
+        });
+    };
+
     const handleInput = (event) => {
-        setValuesExercise(prev => ({ ...prev, [event.target.name]: event.target.value}))
-    }
+        setValuesExercise(prev => ({ ...prev, [event.target.name]: event.target.value }))
+    };
 
     const handleSubmitAddExercise = (event) => {
         event.preventDefault();
-        axios.post('http://localhost:8081/routineExercise', valuesExercise)
+        axios.post('http://localhost:8081/addexercise', valuesExercise)
             .then(res => {
-                navigate(-1);
+                navigate('/home');
             })
             .catch(err => console.log(err));
-    }
+    };
 
     const handleGoBack = () => {
         navigate(-1);
@@ -63,27 +71,49 @@ function AddExercise() {
                         ))}
                     </select>
                     {errors.name && <span className='text-danger'> {errors.name}</span>}
-                    {/*<OptionsForExercise selectedExercise={exerciseType} handleExerciseClick={handleExerciseTypeChange} valuesExercise={valuesExercise} handleInput={handleInput} />*/}
                 </div>
 
                 <Link to='/createexercise' id='defaultButton' type='submit'>Create exercise</Link>
+
                 <div className='prompt'>
-                    <label id='top-text' htmlFor="exercise sets"><strong>Number of sets:</strong></label>
-                    <input name="sets" id='formsInput' type="exercise sets" placeholder='Enter number of sets:' onChange={handleInput}/>
-
-                    <label id='top-text' htmlFor="exercise reps"><strong>Number of reps:</strong></label>
-                    <input name="reps" id='formsInput' type="exercise reps" placeholder='Enter number of reps:' onChange={handleInput}/>
-
-                    <label id='top-text' htmlFor="exercise weight"><strong>Weight:</strong></label>
-                    <input name="weight" id='formsInput' type="exercise weight" placeholder='Enter weight:' onChange={handleInput}/>
-
-                    <label id='top-text' htmlFor="exercise time"><strong>Duration:</strong></label>
-                    <input name="duration" id='formsInput' type="exercise time" placeholder='Enter duration:' onChange={handleInput}/>
+                    <button
+                        className={`exercise-type-button ${exerciseType === 'sets' ? 'active' : ''}`}
+                        onClick={() => handleExerciseTypeChange('sets')}
+                        type="button"
+                    >
+                        Sets & Reps
+                    </button>
+                    <button
+                        className={`exercise-type-button ${exerciseType === 'time' ? 'active' : ''}`}
+                        onClick={() => handleExerciseTypeChange('time')}
+                        type="button"
+                    >
+                        Time
+                    </button>
                 </div>
 
+                {exerciseType === 'sets' && (
+                    <div className='prompt'>
+                        <label id='top-text' htmlFor="exercise sets"><strong>Number of sets:</strong></label>
+                        <input name="sets" id='formsInput' type="number" placeholder='Enter number of sets:' onChange={handleInput}/>
+
+                        <label id='top-text' htmlFor="exercise reps"><strong>Number of reps:</strong></label>
+                        <input name="reps" id='formsInput' type="number" placeholder='Enter number of reps:' onChange={handleInput}/>
+
+                        <label id='top-text' htmlFor="exercise weight"><strong>Weight:</strong></label>
+                        <input name="weight" id='formsInput' type="number" placeholder='Enter weight:' onChange={handleInput}/>
+                    </div>
+                )}
+
+                {exerciseType === 'time' && (
+                    <div className='prompt'>
+                        <label id='top-text' htmlFor="exercise time"><strong>Duration:</strong></label>
+                        <input name="duration" id='formsInput' type="number" placeholder='Enter duration:' onChange={handleInput}/>
+                    </div>
+                )}
+
                 <div className='prompt'>
-                    {/*TODO add handle*/}
-                    <button id='colouredButton' type="submit" onSubmit={handleSubmitAddExercise}>Save exercise</button>
+                    <button type="submit" id='colouredButton' onClick={handleSubmitAddExercise}>Save exercise</button>
                 </div>
             </form>
         </div>
@@ -91,61 +121,3 @@ function AddExercise() {
 }
 
 export default AddExercise;
-
-function OptionsForExercise({selectedExercise, handleExerciseClick, valuesExercise, handleInput}) {
-    const exerciseOptions = ['Exercise with sets', 'Exercise with time'];
-    const convertSecondsToMinutes = (seconds) => {
-        const totalSeconds = parseInt(seconds);
-        if (!isNaN(totalSeconds)) {
-            const minutes = Math.floor(totalSeconds / 60);
-            const remainingSeconds = totalSeconds % 60;
-            return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-        }
-        return '';
-    }
-
-    return (
-        <div className='options-container'>
-            {exerciseOptions.map((option, index) => (
-                <button
-                    key={index}
-                    className={selectedExercise === option ? 'selected-exercise' : ''}
-                    onClick={() => handleExerciseClick(option)}
-                >
-                    {option}
-                </button>
-            ))}
-            {selectedExercise === 'Exercise with sets' && (
-                <>
-                    <div className='prompt'>
-                        <label id='top-text' htmlFor="exercise sets"><strong>Number of sets:</strong></label>
-                        <input id='formsInput' type="exercise sets" placeholder='Enter number of sets:'
-                               name='sets' onChange={handleInput}/>
-                    </div>
-                    <div className='prompt'>
-                        <label id='top-text' htmlFor="exercise reps"><strong>Number of reps:</strong></label>
-                        <input id='formsInput' type="exercise reps" placeholder='Enter number of reps:'
-                               name='reps' onChange={handleInput}/>
-                    </div>
-                    <div className='prompt'>
-                        <label id='top-text' htmlFor="exercise weight"><strong>Weight:</strong></label>
-                        <input id='formsInput' type="exercise weight" placeholder='Enter weight:'
-                               name='weight' onChange={handleInput}/>
-                    </div>
-                </>
-            )}
-
-            {selectedExercise === 'Exercise with time' && (
-                <>
-                    <div className='prompt'>
-                        <label id='top-text' htmlFor="exercise time"><strong>Duration:</strong>
-                            <span>{convertSecondsToMinutes(valuesExercise.time)}</span>
-                        </label>
-                        <input id='formsInput' type="number" placeholder='Enter duration:'
-                               name='duration' value={valuesExercise.time} onChange={handleInput} />
-                    </div>
-                </>
-            )}
-        </div>
-    );
-}
