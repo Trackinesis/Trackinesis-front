346a6fd347c4fa67c1bd6343de85e1ead7d04890
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-05-31 11:49:02 -0300

    personal calendar + plans listed little modifications

--- src/pages/calendar/PersonalCalendar.jsx
+++ src/pages/calendar/PersonalCalendar.jsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import Calendar from 'react-calendar';
-//import 'react-calendar/dist/Calendar.css';
 import axios from 'axios';
 import '../../styles.css';
 
 function PersonalCalendar() {
     const [date, setDate] = useState(new Date());
-    const [events, setEvents] = useState([]);
+    const [plans, setPlans] = useState([]);
 
     useEffect(() => {
-        // Lógica para obtener eventos de la base de datos y establecerlos en el estado de eventos
-        fetchEventsFromDatabase();
+        axios.get('http://localhost:8081/plan')
+            .then(res => {
+                const plansData = res.data;
+                setPlans(plansData);
+            })
+            .catch(err => console.log(err));
     }, []);
 
-    const fetchEventsFromDatabase = async () => {
-        try {
-            const response = await axios.get('ruta/hacia/tus/eventos');
-            setEvents(response.data);
-        } catch (error) {
-            console.error('Error fetching events:', error);
-        }
+    const isDateInRange = (dateToCheck, startDate, endDate) => {
+        return dateToCheck >= new Date(startDate) && dateToCheck <= new Date(endDate);
     };
 
     return (
@@ -29,13 +26,13 @@ function PersonalCalendar() {
                 onChange={setDate}
                 value={date}
                 tileContent={({ date, view }) => {
-                    // Lógica para mostrar contenido en las celdas del calendario
-                    const formattedDate = date.toLocaleDateString(); // Utilizar la función de JavaScript para formatear la fecha
-                    const eventsForDate = events.filter(event => event.date === formattedDate);
+                    const eventsForDate = plans.filter(plan => isDateInRange(date, plan.startDate, plan.endDate));
                     return (
                         <div>
                             {eventsForDate.map(event => (
-                                <div key={event.id}>{event.title}</div>
+                                <div key={event.id}>
+                                    <p>{event.name}</p>
+                                </div>
                             ))}
                         </div>
                     );
