85a0d3cbcb6f8cefc48237b96512e7dfe8905e42
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-07-03 19:57:53 -0300

    working routineExercise

--- src/pages/addExercise/AddExercise.jsx
+++ src/pages/addExercise/AddExercise.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import './AddExercise.css'
-import '../../styles.css'
+import './AddExercise.css';
+import '../../styles.css';
 
 function AddExercise() {
     const navigate = useNavigate();
+    const { routineId } = useParams();
     const [errors, setErrors] = useState({});
     const [valuesExercise, setValuesExercise] = useState({
         name: '',
@@ -13,31 +14,48 @@ function AddExercise() {
         reps: '',
         weight: '',
         duration: '',
-        // routineId: routine.routineId,
-        //exerciseId: '',
+        routineId: routineId, // Establece el routineId aquí
     });
     const [exerciseOptions, setExerciseOptions] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:8081/exercise')
             .then(res => {
-                const exercises = res.data.map(exercise => exercise.name);
+                const exercises = res.data;
                 setExerciseOptions(exercises);
             })
             .catch(err => console.log(err));
     }, []);
 
     const handleInput = (event) => {
-        setValuesExercise(prev => ({ ...prev, [event.target.name]: event.target.value }))
+        setValuesExercise(prev => ({ ...prev, [event.target.name]: event.target.value }));
     };
 
     const handleSubmitAddExercise = (event) => {
         event.preventDefault();
-        axios.post('http://localhost:8081/routineExercise', valuesExercise)
-            .then(res => {
-                navigate('/home');
-            })
-            .catch(err => console.log(err));
+        // Fetch the selected exercise ID based on the exercise name
+        const selectedExercise = exerciseOptions.find(exercise => exercise.name === valuesExercise.name);
+        const exerciseId = selectedExercise ? selectedExercise.exerciseId : null;
+
+        if (exerciseId) {
+            const routineExerciseData = {
+                routineId: valuesExercise.routineId,
+                exerciseId: exerciseId,
+                name: valuesExercise.name, // Asegúrate de incluir el name
+                sets: valuesExercise.sets,
+                reps: valuesExercise.reps,
+                weight: valuesExercise.weight,
+                duration: valuesExercise.duration
+            };
+
+            axios.post('http://localhost:8081/routineExercise', routineExerciseData)
+                .then(res => {
+                    navigate('/home');
+                })
+                .catch(err => console.log(err));
+        } else {
+            console.error('Exercise not found');
+        }
     };
 
     const handleGoBack = () => {
@@ -55,7 +73,7 @@ function AddExercise() {
                     <select name="name" onChange={handleInput} id='formsInput'>
                         <option disabled selected value="">Select Exercise</option>
                         {exerciseOptions.map((exercise, index) => (
-                            <option key={index} value={exercise}>{exercise}</option>
+                            <option key={index} value={exercise.name}>{exercise.name}</option>
                         ))}
                     </select>
                     {errors.name && <span className='text-danger'> {errors.name}</span>}
@@ -96,52 +114,3 @@ function AddExercise() {
 }
 
 export default AddExercise;
-
-// <div className='prompt'>
-//                     <button
-//                         className={`exercise-type-button ${exerciseType === 'sets' ? 'active' : ''}`}
-//                         onClick={() => handleExerciseTypeChange('sets')}
-//                         type="button"
-//                     >
-//                         Sets & Reps
-//                     </button>
-//                     <button
-//                         className={`exercise-type-button ${exerciseType === 'time' ? 'active' : ''}`}
-//                         onClick={() => handleExerciseTypeChange('time')}
-//                         type="button"
-//                     >
-//                         Time
-//                     </button>
-//                 </div>
-//
-//                 {exerciseType === 'sets' && (
-//                     <div className='prompt'>
-//                         <label id='top-text' htmlFor="exercise sets"><strong>Number of sets:</strong></label>
-//                         <input name="sets" id='formsInput' type="number" placeholder='Enter number of sets:' onChange={handleInput}/>
-//
-//                         <label id='top-text' htmlFor="exercise reps"><strong>Number of reps:</strong></label>
-//                         <input name="reps" id='formsInput' type="number" placeholder='Enter number of reps:' onChange={handleInput}/>
-//
-//                         <label id='top-text' htmlFor="exercise weight"><strong>Weight:</strong></label>
-//                         <input name="weight" id='formsInput' type="number" placeholder='Enter weight:' onChange={handleInput}/>
-//                     </div>
-//                 )}
-//
-//                 {exerciseType === 'time' && (
-//                     <div className='prompt'>
-//                         <label id='top-text' htmlFor="exercise time"><strong>Duration:</strong></label>
-//                         <input name="duration" id='formsInput' type="number" placeholder='Enter duration:' onChange={handleInput}/>
-//                     </div>
-//                 )}
-// const handleExerciseTypeChange = (type) => {
-//         setExerciseType(type);
-//         setValuesExercise({
-//             name: '',
-//             sets: '',
-//             reps: '',
-//             weight: '',
-//             duration: '',
-//         });
-//     };
-// const [selectedExercise, setSelectedExercise] = useState('');
-//     const [exerciseType, setExerciseType] = useState(null);
