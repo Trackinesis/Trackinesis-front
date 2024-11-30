cbda03d12774ef383adfe7c1b7c961468befaf5a
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-30 17:06:38 -0300

    took out back button in step two

--- src/pages/signupStepTwo/SignupStepTwo.jsx
+++ src/pages/signupStepTwo/SignupStepTwo.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import Validation from './SignupStepTwoValidation';
 import axios from 'axios';
 import '../../styles.css'
-import './SignupStepTwo.css'
+//import './SignupStepTwo.css'
 import BackButton from "../../components/backButton/BackButton";
 
 function SignupStepTwo() {
@@ -34,13 +34,8 @@ function SignupStepTwo() {
         }
     }
 
-    const handleGoBack = () => {
-        navigate(-1);
-    };
-
     return (
         <div className='main-page p'>
-            <button onClick={handleGoBack} id="backButton"><BackButton/></button>
             <h2 className='main-page-header'>Sign-Up</h2>
 
             <form action="" onSubmit={handleSubmit}>
@@ -74,7 +69,7 @@ function SignupStepTwo() {
                     </select>
                     {errors.gender && <span className='text-danger'> {errors.gender}</span>}
                 </div>
-                <p className='simpleText' >By clicking here you agree with our terms of service.</p>
+                <p>By clicking here you agree with our terms of service.</p>
                 <button type='submit' onClick={handleSubmit} id='colouredButton'>Join the
                     Club!
                 </button>
