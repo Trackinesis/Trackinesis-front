bf841e81d1f8c9988d47a6f413d54ecb086c2b7b
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-12-03 14:19:28 -0300

    betta home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,30 @@
-import { FaRegCalendarAlt, FaUserCircle } from "react-icons/fa"; // Importa ambos íconos
+import {FaRegCalendarAlt, FaRunning, FaUserCircle} from "react-icons/fa"; // Importa ambos íconos
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Home.css';
 import axios from 'axios';
-import '../../styles.css'
+//import '../../styles.css'
 import FooterNavigation from "../../components/footerNavigation/FooterNavigation";
+import card1 from "../../assets/icons/card_1.jpg"
+import card2 from "../../assets/icons/card_2.jpg"
+import card3 from "../../assets/icons/card_3.jpg"
+import card4 from "../../assets/icons/card_4.jpg"
+import card6 from "../../assets/icons/card_6.jpg"
+import card7 from "../../assets/icons/card_7.jpg"
+
 
 function Home() {
     const [name, setName] = useState(null);
     const navigate = useNavigate();
+
+    const imagesUrls = [
+        card1,
+        card2,
+        card3,
+        card4,
+        card6,
+        card7
+    ]
 
     useEffect(() => {
         const fetchName = async () => {
@@ -37,7 +53,7 @@ function Home() {
     const dayName = getDayName(today);
 
     return (
-        <div className='home-page-main-format p'>
+        <div className='home-page-main-format'>
             <FaUserCircle
                 onClick={() => navigate('/userpage')}
                 className='user-icon'
@@ -47,14 +63,19 @@ function Home() {
                 className='calendar-icon'
             />
 
-            <h2 className='main-page-header h2'>Hi {name}</h2>
-            <h3 className='main-page-header h2'>Today is {dayName}</h3>
+            <div className="top-content">
+                <h2 className='main-page-header'>Hi {name}</h2>
+                <h3 className='main-page-header'>Today is {dayName}</h3>
+            </div>
 
-            <Link to='/trainingsession' id='defaultButton' className='p'>
-                Start Exercise
+            <Link to='/trainingsession' id='start-exercise-button' className='start-exercise-button'>
+                <div className="icon-Container">
+                    <FaRunning className="running-icon"/>
+                </div>
+                <div className="text">Start Exercise</div>
             </Link>
 
-            <FooterNavigation />
+            <FooterNavigation/>
         </div>
     );
 }
